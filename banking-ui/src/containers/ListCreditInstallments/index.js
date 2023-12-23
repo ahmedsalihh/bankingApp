26c88588628ee2df83048d07c8837d1b4be6249c
ahmedsalihh <ahmedsalihh@gmail.com>
2023-12-23 04:50:42 +0300

    Add pay installment functionality

--- banking-ui/src/containers/ListCreditInstallments/index.js
+++ banking-ui/src/containers/ListCreditInstallments/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Table from '../../components/Table';
 
 const ListCreditInstallments = () => {
   let { creditId } = useParams();
+  const navigate = useNavigate();
 
   const [installments, setInstallments] = useState(null);
+  const [amount, setAmount] = useState(0);
+  const [selectedInstallment, setSelectedInstallment] = useState(null);
 
   useEffect(() => {
     listCreditInstallments(creditId);
@@ -20,7 +23,28 @@ const ListCreditInstallments = () => {
   };
 
   const handlePayClick = item => {
-    console.log(item);
+    setSelectedInstallment(item);
+  };
+
+  const handlePay = e => {
+    e.preventDefault();
+    const body = {
+      installmentId: selectedInstallment.id,
+      amount,
+    };
+    payInstallment(body);
+  };
+
+  const payInstallment = async body => {
+    const response = await fetch('http://localhost:8080/installments/pay', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    navigate(0);
   };
 
   const columns = [
@@ -48,6 +72,8 @@ const ListCreditInstallments = () => {
       title: 'Actions',
       field: item => (
         <button
+          data-bs-toggle='modal'
+          data-bs-target='#payModal'
           type='button'
           className='btn btn-primary'
           onClick={() => handlePayClick(item)}
@@ -58,7 +84,78 @@ const ListCreditInstallments = () => {
     },
   ];
 
-  return <Table columns={columns} data={installments} />;
+  return (
+    <>
+      <div
+        class='modal fade'
+        id='payModal'
+        tabindex='-1'
+        aria-labelledby='payModal'
+        aria-hidden='true'
+      >
+        <div class='modal-dialog'>
+          <div class='modal-content'>
+            <div class='modal-header'>
+              <h1 class='modal-title fs-5' id='payModal'>
+                Pay Installments
+              </h1>
+              <button
+                type='button'
+                class='btn-close'
+                data-bs-dismiss='modal'
+                aria-label='Close'
+              ></button>
+            </div>
+            <div class='modal-body'>
+              <form>
+                <div className='mb-3'>
+                  <label htmlFor='name' className='form-label'>
+                    Id
+                  </label>
+                  <input
+                    disabled
+                    className='form-control'
+                    id='amount'
+                    value={selectedInstallment && selectedInstallment.id}
+                  />
+                </div>
+                <div className='mb-3'>
+                  <label htmlFor='name' className='form-label'>
+                    Amount
+                  </label>
+                  <input
+                    onChange={e => setAmount(e.target.value)}
+                    className='form-control'
+                    id='amount'
+                    value={amount}
+                    placeholder='Enter your name'
+                  />
+                </div>
+              </form>
+            </div>
+            <div class='modal-footer'>
+              <button
+                type='button'
+                class='btn btn-secondary'
+                data-bs-dismiss='modal'
+              >
+                Cancel
+              </button>
+              <button
+                type='button'
+                class='btn btn-primary'
+                onClick={handlePay}
+                data-bs-dismiss='modal'
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Table columns={columns} data={installments} />
+    </>
+  );
 };
 
 export default ListCreditInstallments;
